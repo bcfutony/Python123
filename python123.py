2d346100f28b70306484b93a5e8bb781096c066c
bcfutony <101447795+bcfutony@users.noreply.github.com>
2023-11-29 12:55:05 +0700

    Add files via upload

---
+++ python123.py
@@ -0,0 +1,11 @@
+print("hello python123 git")
+#dictionary list...
+dsSP = ["ong", "phu tung", "keo dan"]
+
+for sp in dsSP:
+    print(f"ds sp la: {sp}")
+
+for k, value in enumerate(dsSP):
+    print(f"ds sp la: {value}, vi tri: {k}")
+
+#abc
